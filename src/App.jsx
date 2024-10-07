cfd4e5a2db797c7934fc868725cdffb8cc76480d
gunaasin <guna.asin06@gmail.com>
2024-10-07 18:33:29 +0530

    version 5.5

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Frame } from "./Frame";
 function App() {
   
   return (
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={1} py={1}>
 <Frame/>
      
     </Box>
